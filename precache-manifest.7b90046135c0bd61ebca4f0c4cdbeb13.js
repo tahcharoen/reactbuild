d867973ba0ab75b44ebe4642e852b66f54d27151
Tah Developer For Your <55322063+tahcharoen@users.noreply.github.com>
2022-05-19 09:55:57 +0700

    BuildBeginer
    BuildBeginer

---
+++ precache-manifest.7b90046135c0bd61ebca4f0c4cdbeb13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8364b2c272f3d5a0c2a",
+    "url": "/static/css/main.85b71a2c.chunk.css"
+  },
+  {
+    "revision": "e8364b2c272f3d5a0c2a",
+    "url": "/static/js/main.e8364b2c.chunk.js"
+  },
+  {
+    "revision": "a5456eae47356bfb2ce3",
+    "url": "/static/js/1.a5456eae.chunk.js"
+  },
+  {
+    "revision": "5c8b9505e8c82eec1ab8",
+    "url": "/static/css/2.4c56226a.chunk.css"
+  },
+  {
+    "revision": "5c8b9505e8c82eec1ab8",
+    "url": "/static/js/2.5c8b9505.chunk.js"
+  },
+  {
+    "revision": "dfd48835b63c68f1a97d",
+    "url": "/static/js/runtime~main.dfd48835.js"
+  },
+  {
+    "revision": "ea5232cd20a6caab1b4f7d47baccb43d",
+    "url": "/index.html"
+  }
+];
